8c1f1cd941bb370626aad4ba799df49be8e4db91
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-06-01 22:51:10 +0530

    Update ReverseWords.py

--- ReverseWords.py
+++ ReverseWords.py
@@ -1,19 +1,3 @@
-Reverse a String
-You are given a string s. You need to reverse the string.
-
-Example 1:
-
-Input:
-s = Geeks
-Output: skeeG
-
-Example 2:
-
-Input:
-s = for
-Output: rof
-
-#User function Template for python3
 
 def reverseWord(s):
     #your code here
