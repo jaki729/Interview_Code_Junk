f57aeb9a6d1cdce06503ad16fc6454e0075c4e4f
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-07-07 00:22:58 +0530

    Update Fibinacci.py

--- Fibinacci.py
+++ Fibinacci.py
@@ -1,3 +1,4 @@
+#m1
 def fib(n):
     if(n<=1):
         return n
@@ -6,3 +7,27 @@ def fib(n):
 print('Enter a number: ', end=' ')
 num=int(input())
 print('Nth fibonacci number is: ',fib(num))
+
+#m2
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        if n==0:
+            return a
+        elif n==1:
+            return b
+        else:
+            for i in range(1, n):
+                c = a + b
+                a = b
+                b = c
+            return b
+ 
+#m3
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
+        else:
+            return (self.fib(n-1)+self.fib(n-2))
