8ae7ce062c00e0dbb256a0d9192c815d8efa6282
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-10-15 02:21:06 +0530

    Update 2Sum.py

--- Start-Problems-I/2Sum.py
+++ Start-Problems-I/2Sum.py
@@ -1,3 +1,15 @@
+#m1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap={}
+        for i,n in enumerate(nums):
+            difference =target-n
+            if difference in hashmap:
+                return [hashmap[difference],i]
+            hashmap[n]=i
+        return
+
+#m2
 import itertools
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
