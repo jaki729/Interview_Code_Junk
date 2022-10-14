f2fe0f8ef10230e0ab7939fdf2e9180f9988014b
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-10-15 02:06:14 +0530

    Create ValidAnagram.py

---
+++ Start-Problems-I/ValidAnagram.py
@@ -0,0 +1,18 @@
+#m1
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
+ 
+#m2
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        count_s,count_t={},{}
+        for i in range(len(s)):
+            count_s[s[i]]=1+count_s.get(s[i],0)
+            count_t[t[i]]=1+count_t.get(t[i],0)
+        for c in count_s:
+            if count_s[c]!=count_t.get(c,0):
+                return False
+        return True
