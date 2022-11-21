fcdacf8f63582d48564348ea1c3f766197956e6b
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-11-21 15:01:34 +0530

    Create 88. Merge Sorted Array.py

---
+++ CODE_ROUND-1/88. Merge Sorted Array.py
@@ -0,0 +1,38 @@
+'''
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+'''
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        #last index nums1
+        last=m+n-1
+        #merge in reverse order
+        while m>0 and n>0:
+            if nums1[m-1]>nums2[n-1]:
+                nums1[last]=nums1[m-1]
+                m-=1
+            else:
+                nums1[last]=nums2[n-1]
+                n-=1
+            last-=1
+        #now fill leftover elemnts of nums2 
+        while n>0:
+            nums1[last]=nums2[n-1]
+            n,last=n-1,last-1
