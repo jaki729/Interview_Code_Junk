3133b4757b67309daea2bb1b0221ef68c369b528
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-11-21 14:35:16 +0530

    Update 268. Missing Number.py

--- CODE_ROUND-1/268. Missing Number.py
+++ CODE_ROUND-1/268. Missing Number.py
@@ -1,3 +1,13 @@
+#Example 1:
+#Input: nums = [3,0,1]
+#Output: 2
+#Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+#Example 2:
+#Input: nums = [0,1]
+#Output: 2
+#Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         ans=len(nums)
