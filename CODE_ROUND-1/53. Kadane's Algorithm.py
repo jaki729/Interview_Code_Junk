4d9c89ef1c4ca333f2d7e9dc1532ef1a2b930a89
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-11-21 15:18:59 +0530

    Create 53. Kadane's Algorithm.py

---
+++ CODE_ROUND-1/53. Kadane's Algorithm.py
@@ -0,0 +1,25 @@
+#https://leetcode.com/problems/maximum-subarray/
+'''
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+
+Example 3:
+Input: nums = [5,4,-1,7,8]
+Output: 23
+'''
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub=nums[0]
+        cur_sum=0
+        for i in nums:
+            if cur_sum<0:
+                cur_sum=0
+            cur_sum+=i
+            max_sub=max(max_sub,cur_sum)
+        return max_sub
