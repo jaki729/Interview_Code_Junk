e4b31a00d96818f8eb141a4c784defba43c5364d
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-11-21 23:34:42 +0530

    Update 75. Sort Colors.py

--- CODE_ROUND-1/75. Sort Colors.py
+++ CODE_ROUND-1/75. Sort Colors.py
@@ -1,11 +1,12 @@
-#Example 1:
-#Input: nums = [2,0,2,1,1,0]
-#Output: [0,0,1,1,2,2]
+'''
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
 
-#Example 2:
-#Input: nums = [2,0,1]
-#Output: [0,1,2]
-
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+'''
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         l, r = 0,len(nums)-1
@@ -25,3 +26,6 @@ class Solution:
                 r-=1
                 i-=1
             i+=1
+'''
+The technique used is quick sort parition 
+'''
