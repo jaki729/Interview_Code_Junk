a853208cc064f58b7a9d53e2ffb878f2dd76428a
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2022-12-18 12:49:53 +0530

    Create p1.py

---
+++ STRIVER_A2Z_SHEET/GFG/PATTERNS/p1.py
@@ -0,0 +1,32 @@
+'''
+Input: 5
+
+Output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+#User function Template for python3
+
+class Solution:
+    def printSquare(self, N):
+        # Code here
+        for i in range(N):
+            for j in range(N):
+                print("*",end=" ")
+            print()
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printSquare(N)
+# } Driver Code Ends
