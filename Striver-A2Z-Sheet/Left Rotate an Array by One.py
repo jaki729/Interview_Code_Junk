c52e2617a9df408f76f284c88aadc749142290ce
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2023-07-20 22:03:48 +0530

    Delete Left Rotate an Array by One.py

--- Striver-A2Z-Sheet/Left Rotate an Array by One.py
+++
@@ -1,8 +0,0 @@
-'''
-https://www.codingninjas.com/studio/problems/left-rotate-an-array-by-one_5026278?leftPanelTab=0
-'''
-
-def rotateArray(arr: [], n: int) -> []:
-    # Write your code from here.
-    return arr[1:]+arr[:1]
-
