fabeb7625a89e1a47108683027f7bad385c1f56d
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2023-08-13 21:31:15 +0530

    Update 7. Reverse Integer.py

--- Striver-A2Z-Sheet/LeetCode/7. Reverse Integer.py
+++ Striver-A2Z-Sheet/LeetCode/7. Reverse Integer.py
@@ -5,15 +5,21 @@ The time complexity of the solution is O(log10(x)).
 '''
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0 :
+        if x < 0:
             sign = -1
-            x = -x
+            x = -x  # Make x positive for reverse calculation
         else:
             sign = 1
-        rev_digit = 0
+        
+        rev_digit = 0  # Initialize the reversed number
+        
         while x > 0:
+            # Extract the last digit of x and add it to the reversed number
             rev_digit = rev_digit * 10 + x % 10
-            x //= 10
-        if rev_digit > 2147483647:
-            return 0
-        return sign * rev_digit
+            x //= 10  # Move to the next digit
+        
+        if rev_digit > 2147483647:  # Check for integer overflow
+            return 0  # Return 0 if overflow occurs
+        
+        return sign * rev_digit  # Return the reversed number with the original sign
+
